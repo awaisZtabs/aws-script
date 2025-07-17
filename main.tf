5cd13d18fa68c412a9ecbdb35b9c34c76e001c66
awaiss <awais@apimio.com>
2025-07-18 00:38:01 +0200

    added vars

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+region = var.aws_region
+}
+
+resource "aws_key_pair" "main" {
+key_name = var.key_name
+public_key = file(var.public_key_path)
+}
+
+resource "aws_security_group" "turn_sg" {
+name = "turn-sg"
+description = "Allow SSH access"
+
+ingress {
+from_port = 22
+to_port = 22
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+egress {
+from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_blocks = ["0.0.0.0/0"]
+}
+}
+
+resource "aws_instance" "turn_ec2" {
+ami = var.ami_id
+instance_type = var.instance_type
+key_name = aws_key_pair.main.key_name
+vpc_security_group_ids = [aws_security_group.turn_sg.id]
+user_data = file("userdata.sh")
+
+tags = {
+Name = "turn-server"
+}
+}
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "aws_region" {
-default = "eu-central-1"
+default = "us-east-1"
 }
 
 variable "ami_id" {
-default = "ami-0cc293023f983ed53"
+default = "ami-0150ccaf51ab55a51"
 }
 
 variable "instance_type" {
@@ -11,10 +11,10 @@ default = "t3.nano"
 }
 
 variable "key_name" {
-description = "Name for EC2 key pair"
+description = "turn-key"
 }
 
 variable "public_key_path" {
-description = "Path to your SSH public key (e.g., ~/.ssh/id_rsa.pub)"
+description = "~/.ssh/turn-key.pub"
 }
 
