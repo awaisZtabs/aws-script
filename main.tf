526b0e6833b05da6141d538cc4a93418b009ea6c
awaiss <awais@apimio.com>
2025-07-19 18:55:48 +0200

    added

--- main.tf
+++ main.tf
@@ -74,20 +74,20 @@ resource "aws_dynamodb_table" "messages" {
   }
 }
 
-resource "aws_iam_role" "lambda_exec_role" {
-  name = "lambda-exec-role"
+# resource "aws_iam_role" "lambda_exec_role" {
+#   name = "lambda-exec-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Action = "sts:AssumeRole"
+#       Effect = "Allow"
+#       Principal = {
+#         Service = "lambda.amazonaws.com"
+#       }
+#     }]
+#   })
+# }
 
 # resource "aws_iam_role_policy_attachment" "git" {
 #   role       = aws_iam_role.lambda_exec_role.name
