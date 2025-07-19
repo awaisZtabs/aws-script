2ce06dc4b77634a0f3310288950dee2fd27940e5
awaiss <awais@apimio.com>
2025-07-19 18:46:03 +0200

    added lambda

--- lambda/s3_logger/handler.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def lambda_handler(event, context):
-    print("Received event:")
-    print(json.dumps(event, indent=2))
-    return {
-        'statusCode': 200,
-        'body': json.dumps('S3 Upload Triggered Lambda Executed')
-    }
--- main.tf
+++ main.tf
@@ -74,4 +74,31 @@ resource "aws_dynamodb_table" "messages" {
   }
 }
 
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda-exec-role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_exec" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "hello_lambda" {
+  function_name = "HelloLambda"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs18.x"
+  filename      = "${path.module}/lambda/hello_lambda.zip"
+  source_code_hash = filebase64sha256("${path.module}/lambda/hello_lambda.zip")
+}
